ddb6438ecbd54c13abf218fdf5c7cbe7bbe2c286
Conte <alexey.kolesnikov77@gmail.com>
2024-10-04 11:32:55 +0300

    Добавление ctrl+w чтобы стабильнее работало

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,7 @@ Set-PSReadLineKeyHandler -Chord Ctrl+z -Function Undo
 Set-PSReadLineKeyHandler -Chord Ctrl+c -Function Copy
 Set-PSReadLineKeyHandler -Chord Ctrl+x -Function Cut
 Set-PSReadLineKeyHandler -Chord Ctrl+Backspace -Function BackwardKillWord
+Set-PSReadLineKeyHandler -Chord Ctrl+w -Function BackwardKillWord
 Set-PSReadLineKeyHandler -Chord Ctrl+a -Function SelectAll
 Set-PSReadLineKeyHandler -Chord Shift+Ctrl+RightArrow -Function SelectNextWord
 Set-PSReadLineKeyHandler -Chord Shift+Ctrl+LeftArrow -Function SelectBackwardWord
